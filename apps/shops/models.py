40eed6f7c06c559e13ad190e2a2eae67c16f536b
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2024-07-16 11:20:30 +0500

    created shops / Country, Language, ShopCategory, Currency

--- apps/shops/models.py
+++ apps/shops/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, verbose_name='nomi')
+
+    class Meta:
+        verbose_name = 'Davlat'
+        verbose_name_plural = 'Davlatlar'
+
+
+class Language(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Nomi')
+    code = models.CharField(max_length=255, verbose_name='Kodi')
+    icon = models.CharField(max_length=255, verbose_name='Belgisi')
+
+
+class ShopCategory(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nomi')
+
+    class Meta:
+        verbose_name = 'Do\'kon toifasi'
+        verbose_name_plural = 'Do\'kon toifalari'
+
+    def __str__(self):
+        return self.name
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Nomi')
+    order = models.PositiveSmallIntegerField(default=1, db_default=1, verbose_name='Rangi')
+
+    class Meta:
+        verbose_name = 'Pul birligi'
+        verbose_name_plural = 'Pul birliklari'
+
+    def __str__(self):
+        return self.name
